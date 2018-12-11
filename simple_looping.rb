7c7a8108bac6eb32fc7b94fdb0aad5289c91a2fa
s greenbaum <gbs4ever2@gmail.com>
2018-12-11 20:54:46 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,17 +3,25 @@ def loop_iterator(number_of_times)
   counter= 0
 loop  do
 puts  "Welcome to Flatiron School's Web Development Course!"
+<<<<<<< HEAD
     counter =+1
+=======
+    counter +=1
+>>>>>>> 0e967c7b06b3d16e4061596affb0369b38ffd684
   break if counter ==number_of_times
 end
 end
 
+<<<<<<< HEAD
   # Maybe we should keep count of the number of times we've puts out the
+=======
+>>>>>>> 0e967c7b06b3d16e4061596affb0369b38ffd684
 
  ## phrase = "Welcome to Flatiron School's Web Development Course!"
 def times_iterator(number_of_times)
 number_of_times.times do   # code your solution here using the "times" keyword
   puts "Welcome to Flatiron School's Web Development Course!"
+<<<<<<< HEAD
 end
 end
 #def while_iterator(number_of_times)
@@ -37,3 +45,32 @@ end
 #  phrase = "Welcome to Flatiron School's Web Development Course!"
 
 #end
+=======
+end
+end
+
+def while_iterator(number_of_times)
+  counter =0
+while counter <number_of_times
+  puts   "Welcome to Flatiron School's Web Development Course!"
+counter +=1
+
+end
+end
+def until_iterator(number_of_times)
+  counter =0
+until counter == number_of_times
+   puts   "Welcome to Flatiron School's Web Development Course!"
+counter +=1
+end
+end
+
+def for_iterator(number_of_times)
+
+for counter in 1..number_of_times
+
+puts "Welcome to Flatiron School's Web Development Course!"
+
+end
+end
+>>>>>>> 0e967c7b06b3d16e4061596affb0369b38ffd684
