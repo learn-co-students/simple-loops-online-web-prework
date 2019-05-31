8b5ad2f477ea36856e313f00f5b2738d67b412c4
Adam Ervin <adamjervin@gmail.com>
2019-05-31 17:49:12 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,13 +1,19 @@
-
-def loop_iterator(num_of_times)
-  counter = 0
- loop do
- puts "Welcome to Flatiron School's Web"
- counter += 1
- break if counter >=7
- 
-   
+  def loop_iterator(number_of_times)
+  number_of_times = 0
+  loop do 
+    number_of_times += 1
+    puts "Welcome to Flatiron School's Web Development Course!"
+    if number_of_times >= 7
+      break
+    end
+  end
 end
+  
+  
+  
+
+
+
 
  
  
@@ -23,35 +29,43 @@ end
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
-  phrase = (number_of_times)
-  
-end
+
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  count = 0
+  7.times do
+   puts  "Welcome to Flatiron School's Web Development Course!"
   
+end
 end
 
 def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+counter = 0
+while counter < 7
+counter += 1
+puts "Welcome to Flatiron School's Web Development Course!"
+
+end
 end
 
-def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
 
+def until_iterator(number_of_times)
+  counter = 0
+ until counter == 7
+  puts"Welcome to Flatiron School's Web Development Course!"
+counter += 1
+end
 end
 
 def for_iterator(number_of_times)
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
+ counter = 1..7
+
+for count in counter do
   
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+ puts "Welcome to Flatiron School's Web Development Course!"
+ 
+end 
 end
+
+
 
